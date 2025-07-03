a4e0ffb0dabfecb44e5ba06b4cbaa3aba571b288
NuttoTL9 <arenasrull00@gmail.com>
2025-07-03 15:19:42 +0700

    add security for database

--- FastAPI/main_fastapi.py
+++ FastAPI/main_fastapi.py
@@ -5,9 +5,18 @@ from starlette.middleware.cors import CORSMiddleware
 from databases import Database
 import datetime
 import asyncpg
+from dotenv import load_dotenv
+import os
 
 # ------------------ Configurations ------------------ #
-POSTGRES_ADMIN_URL = "postgresql://Nutto:099768180Nn_@192.168.1.27:5432/postgres"
+load_dotenv()
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+POSTGRES_ADMIN_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 DATABASE_URL = POSTGRES_ADMIN_URL
 
 # ------------------ FastAPI Init ------------------ #
