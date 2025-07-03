b01777da5d239c3623aabe0da339cada60f75607
BigDev <sukree5284@gmail.com>
2025-07-03 13:54:19 +0700

    add more

---
+++ Client/milvus_grpc_ultils.py
@@ -0,0 +1,44 @@
+import grpc
+import image_transform_pb2
+import image_transform_pb2_grpc
+from pymilvus import connections, Collection, list_collections
+
+def show_collections():
+    connections.connect(alias="default", host="192.168.1.27", port="19530")
+    collections = list_collections()
+    print(f"Collections in Milvus: {collections}")
+    return collections
+
+def show_collection_schema(collection_name):
+    collection = Collection(collection_name)
+    print(f"Schema for '{collection_name}':")
+    for field in collection.schema.fields:
+        print(f"- {field.name} ({field.dtype})")
+
+def get_vector_from_milvus(collection_name, id):
+    collection = Collection(collection_name)
+    results = collection.query(expr=f"id == '{id}'", output_fields=["embedding"])
+    if results:
+        return results[0]["embedding"]
+    else:
+        raise Exception("Vector not found")
+
+def show_all_ids(collection_name):
+    collection = Collection(collection_name)
+    count = collection.num_entities
+    print(f"Total entities: {count}")
+    if count > 0:
+        results = collection.query(expr="", output_fields=["id"], limit=count)
+        print("All IDs in collection:", [r["id"] for r in results])
+    else:
+        print("No entities found in the collection.")
+
+def encode_vector_with_grpc(vector):
+    with open('server.crt', 'rb') as f:
+        trusted_certs = f.read()
+    credentials = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+    channel = grpc.secure_channel('192.168.1.27:50051', credentials)
+    stub = image_transform_pb2_grpc.EncodeServiceStub(channel)
+    request = image_transform_pb2.VectorRequest(vector=vector)
+    response = stub.EncodeVector(request)
+    return response.vector
